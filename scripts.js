d61abd1c04f2e8c32f6efdf7ca7e80e0b3a978f7
cl02ara <aclaramaria06@gmail.com>
2025-01-11 14:06:16 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,6 @@
+// Se quiser adicionar mais interações ou efeitos, pode usar este arquivo.
+
+// Exemplo: Mostrar um alerta ao clicar no botão de fotos
+document.querySelector('.cta-button').addEventListener('click', function() {
+    alert('Aqui estão as nossas fotos especiais!');
+});
